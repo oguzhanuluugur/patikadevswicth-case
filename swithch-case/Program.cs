1ac8ee3d8eecbde3e12e51bd7452c8c2dfa3efce
Oğuzhan Uluuğur <oguzhanuluugur345@gmail.com>
2023-02-05 21:05:02 +0300

    patikadev switch-case
    patika dev c# dersinden switch-case ödevi

---
+++ swithch-case/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace swithch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            //experssion
+            switch(month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasın ");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasın");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasın");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasın");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Veri Girişi");
+                    break;
+
+            }
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Aylarındasınız ");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkBahrar Aylarındasınız ");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Aylarındasınız ");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Aylarındasınız ");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Girdiniz :)");
+                    break;
+
+            }
+        }
+    }
+}
